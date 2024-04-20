c74bff27ef71822328b006c26d1d8fc63459296c
isidro hernandez <isidro@termsignal.com>
2024-04-19 17:14:33 -0700

    pomodor notifications broken

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Exit Insert Mode
+vim.api.nvim_set_keymap("i", "jh", "<Esc>", { noremap = true, silent = true })
+
+-- Save Command Insert mode
+vim.api.nvim_set_keymap("i", "<C-l>", "<Esc>:w<CR>a", { noremap = true, silent = true })
+
+-- Start Pomodoro Timer
+vim.api.nvim_set_keymap("n", "<C-p>", ":TimerStart 25m Coding<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-b>", ":TimerStart 5s Break<CR>", { noremap = true, silent = true })
